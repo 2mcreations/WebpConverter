c7bc602788b455e8f2e0ecc8fac51e561d54a338
Mattia Calore <mattia@2mcreations.it>
2025-07-15 12:28:19 +0200

    install

--- install-webpconvert.sh
+++ install-webpconvert.sh
@@ -4,6 +4,7 @@
 SCRIPT_URL="https://raw.githubusercontent.com/2mcreations/WebpConverter/refs/heads/main/webpconvert"
 
 # Destinazione
+TEMP="/tmp/webpconvert"
 DEST="/usr/local/bin/webpconvert"
 
 echo "🔽 Scaricamento di webpconvert da:"
@@ -11,15 +12,15 @@ echo "$SCRIPT_URL"
 
 # Scarica con curl o wget
 if command -v curl >/dev/null 2>&1; then
-  sudo curl -fsSL "$SCRIPT_URL" -o "$DEST"
+  sudo curl -fsSL "$SCRIPT_URL" -o "$TEMP"
 elif command -v wget >/dev/null 2>&1; then
-  sudo wget -q "$SCRIPT_URL" -O "$DEST"
+  sudo wget -q "$SCRIPT_URL" -O "$TEMP"
 else
   echo "❌ Né curl né wget trovati. Installane uno per continuare."
   exit 1
 fi
 
-# Rende eseguibile
+sudo mv "$TEMP" "$DEST"
 sudo chmod +x "$DEST"
 
 echo "✅ Installato con successo in: $DEST"
